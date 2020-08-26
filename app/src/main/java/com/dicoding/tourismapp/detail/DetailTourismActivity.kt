65914d7e6e3ab68fbc5c55b37c59e12af0eb9659
anggastudio <aa.angga.pratama@gmail.com>
2020-08-27 01:40:49 +0700

    added domain model Tourism.kt

--- app/src/main/java/com/dicoding/tourismapp/detail/DetailTourismActivity.kt
+++ app/src/main/java/com/dicoding/tourismapp/detail/DetailTourismActivity.kt
@@ -6,8 +6,8 @@ import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.dicoding.tourismapp.R
-import com.dicoding.tourismapp.core.data.source.local.entity.TourismEntity
 import com.dicoding.tourismapp.core.ui.ViewModelFactory
+import com.dicoding.tourismapp.domain.model.Tourism
 import kotlinx.android.synthetic.main.activity_detail_tourism.*
 import kotlinx.android.synthetic.main.content_detail_tourism.*
 
@@ -27,11 +27,11 @@ class DetailTourismActivity : AppCompatActivity() {
         val factory = ViewModelFactory.getInstance(this)
         detailTourismViewModel = ViewModelProvider(this, factory)[DetailTourismViewModel::class.java]
 
-        val detailTourism = intent.getParcelableExtra<TourismEntity>(EXTRA_DATA)
+        val detailTourism = intent.getParcelableExtra<Tourism>(EXTRA_DATA)
         showDetailTourism(detailTourism)
     }
 
-    private fun showDetailTourism(detailTourism: TourismEntity?) {
+    private fun showDetailTourism(detailTourism: Tourism?) {
         detailTourism?.let {
             supportActionBar?.title = detailTourism.name
             tv_detail_description.text = detailTourism.description
